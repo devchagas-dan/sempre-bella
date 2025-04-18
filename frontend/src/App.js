543eed19eec2ce2002f43e85b657aee71bf5afe2
devchagas-dan <dannyella.cardoso@hotmail.com>
2025-04-18 15:30:27 -0300

    working on navbar of my store homepage

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './customer/components/Navigation/Navigation';
+
+function App() {
+  return (
+    <div className="">
+      <Navigation/>
+      <div>
+        Home
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
